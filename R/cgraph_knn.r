3c80cce400e6d32aa128b48acf5f3bcfd3e47c31
amostanay <amos.tanay@weizmann.ac.il>
2018-06-11 08:56:47 +0300

    Fix an import bug (as.data.frame...) to put vignette back to work. Also some small addition (allowing creating a graph from raw, non balanced, correlations)

--- R/cgraph_knn.r
+++ R/cgraph_knn.r
@@ -22,6 +22,32 @@ mcell_add_cgraph_from_mat_bknn = function(mat_id, gset_id, graph_id, K, dsamp=F)
 
 	scdb_add_cgraph(graph_id, tgCellGraph(gr, cnames))
 }
+#' Build a cell graph using raw knn graph on given gene features
+#'
+#' @param mat_id matrix object id
+#' @param gset_id gset object id defining the features used for distances
+#' @param graph_id  new graph id to create
+#' @param K the guideline Knn parameter. The balanced will be constructed aiming at K edges per cell
+#'
+#' @export
+
+mcell_add_cgraph_from_mat_raw_knn = function(mat_id, gset_id, graph_id, K, dsamp=F)
+{
+	feat = gset_get_feat_mat(gset_id, mat_id, downsamp=dsamp, add_non_dsamp=dsamp)
+	k_nonz_exp = get_param("scm_k_nonz_exp")
+	feat = log2(1+k_nonz_exp*as.matrix(feat))
+
+	message("willl build raw knn graph on ", ncol(feat), " cells and ", nrow(feat), " genes, this can be a bit heavy for >20,000 cells")
+
+	mcor = tgs_cor(x=feat)
+	x_knn = tgs_knn(mcor, K)
+	gr = x_knn[,c("col1", "col2")]
+	gr$w = 1-(x_knn$rank-1)/K
+	cnames = colnames(feat)
+	colnames(gr) = c("mc1","mc2","w")
+
+	scdb_add_cgraph(graph_id, tgCellGraph(gr, cnames))
+}
 #' Compute a cell cell correlation matrix using features defined by a gene set
 #'
 #' @param mat_id matrix object id

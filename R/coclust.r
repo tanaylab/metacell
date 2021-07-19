cd3c81d8ff1f61aff131e0d65df299a7b7af88c0
Akhiad Bercovich <akhiad@n98.mcl4.weizmann.ac.il>
2021-07-19 14:51:54 +0300

    fixing the non constant table behaviour for filt_by_degree

--- R/coclust.r
+++ R/coclust.r
@@ -66,9 +66,16 @@ mcell_coclust_filt_by_k_deg = function(coc_id, K, alpha)
 	
 	deg_wgt = as.matrix(table(c(edges$node1, edges$node2), c(edges$cnt,edges$cnt)))
 	deg_cum = t(apply(deg_wgt, 1, function(x) cumsum(rev(x))))
-	thresh_K = rowSums(deg_cum > K)
-
-	names(thresh_K) <- levels(edges$node1)
+	thresh_Kr = rowSums(deg_cum > K)
+	lev2int = 1:length(levels(edges$node1))
+	thresh_K = rep(NA, length(levels(edges$node1)))
+	names(thresh_K) = levels(edges$node1)
+	if (is.na(sum(as.numeric(names(thresh_Kr))))){
+		thresh_K[names(thresh_Kr)] = thresh_Kr
+	}
+	else {
+		thresh_K[as.numeric(names(thresh_Kr))] = thresh_Kr
+	}
 
 	filt_edges = thresh_K[edges$node1] < edges$cnt * alpha | 
 							thresh_K[edges$node2] < edges$cnt * alpha
